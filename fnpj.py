b6ea300e40470805d2e0023d2b0aae0a0323db61
zfp <zfp@umich.edu>
2018-04-15 10:18:17 -0400

    v2

--- fnpj.py
+++ fnpj.py
@@ -1,4 +1,4 @@
-
+import secrets
 from bs4 import BeautifulSoup
 import requests
 import sqlite3
@@ -12,7 +12,7 @@ from lyft_rides.client import LyftRidesClient
 
 map_api='AIzaSyCDFrR1bowszXuQLUG8f3pj61Q_Uc_mnzA'
 # 1.cache
-CACHE_FNAME = 'checkcache.json'
+CACHE_FNAME = 'finalcache.json'
 try:
     cache_file = open(CACHE_FNAME, 'r')
     cache_contents = cache_file.read()
@@ -50,45 +50,33 @@ def make_request_using_cache(url,params=None):
         return CACHE_DICTION[unique_ident]
 
 # 2 WRITE INTO DB
-
-
-DBNAME='Foddie_Map.sqlite'
+DBNAME='Foodie_Go.sqlite'
 conn = sqlite3.connect(DBNAME)
 cur = conn.cursor()
-#
+# #
 # statement = '''
 #     DROP TABLE IF EXISTS 'EAT';
 # '''
 # cur.execute(statement)
+# #
+# statement = '''
+#     DROP TABLE IF EXISTS 'RIDE';
+# '''
+# cur.execute(statement)
 #
-statement = '''
-    DROP TABLE IF EXISTS 'RIDE';
-'''
-cur.execute(statement)
-
 # statement='''CREATE TABLE EAT(
 # 'Id' INTEGER NULL PRIMARY KEY AUTOINCREMENT,
 # 'Name' TEXT NOT NULL,
 # 'Phone' TEXT,
 # 'Price' TEXT,
 # 'Rating ' NUMERIC,
-# 'address' TEXT,
-# 'lag_log' TEXT
+# 'Address' TEXT,
+# 'Lag_log' TEXT
 # )'''
 # cur.execute(statement)
 # conn.commit()
 
-statement='''CREATE TABLE RIDE(
-'Id' INTEGER NULL PRIMARY KEY AUTOINCREMENT,
-'Start' TEXT NOT NULL,
-'Desitination' TEXT NOT NULL,
-'Estimated time (minute)' TEXT,
-'Estimated distance(miles)' TEXT,
-'Estimated maximun cost (dollar)' TEXT,
-'Estimated minimum cost (dollar)' TEXT)
-'''
-cur.execute(statement)
-conn.commit()
+
 
 # 3.1 google maps
 class google_map():
@@ -111,8 +99,7 @@ class google_map():
         loc="{},{}".format(lat,log)
 
         return loc
-        #
-        # #2
+
 
     # def nearby(address):
 
@@ -127,43 +114,65 @@ class google_map():
 
 
 # 3. yelp data
-# user_input='San Francisco'
-# baseurl="https://www.yelp.com/"
-# search_url=baseurl+'search'
-# param={}
-# param['find_desc']="Top+100+Places+to+Eat"
-# param['find_loc']=user_input
-# n=range(20)
-#
-# count=0
-# while count<100:
-#     for i in n:
-#         param['start']=i*10
-#         html=make_request_using_cache(search_url,param)
-#         soup_a = BeautifulSoup(html, 'html.parser')
-#         list_of_eat=soup_a.find_all("li",class_="regular-search-result")
-#
-#         for eat in list_of_eat:
-#             try:
-#                 name=eat.find('a',class_='biz-name').text.strip()
-#                 phone=eat.find('span',class_='biz-phone').text.strip()
-#                 price=eat.find('span',class_="business-attribute price-range").text.strip()
-#                 rating=eat.find('div',class_='i-stars')['title'][:3]
-#                 add=eat.find('address').text.strip()
-#                 lat_log=google_map.lat_log(add)
-#                 count+=1
-#
-#                 params=(name,phone,price,rating,add,lat_log)
-#                 statement='''INSERT INTO EAT VALUES(Null,?,?,?,?,?,?)'''
-#                 cur.execute(statement,params)
-#                 conn.commit()
-#
-#             except:
-#                 pass
+user_input='San Francisco'
+baseurl="https://www.yelp.com/"
+search_url=baseurl+'search'
+param={}
+param['find_desc']="Top+100+Places+to+Eat"
+param['find_loc']=user_input
+n=range(20)
+
+count=0
+while count<100:
+    for i in n:
+        param['start']=i*10
+        html=make_request_using_cache(search_url,param)
+        soup_a = BeautifulSoup(html, 'html.parser')
+        list_of_eat=soup_a.find_all("li",class_="regular-search-result")
+
+        for eat in list_of_eat:
+            try:
+                name=eat.find('a',class_='biz-name').text.strip()
+                phone=eat.find('span',class_='biz-phone').text.strip()
+                price=eat.find('span',class_="business-attribute price-range").text.strip()
+                rating=eat.find('div',class_='i-stars')['title'][:3]
+                add=eat.find('address').text.strip()
+                lat_log=google_map.lat_log(add)
+                count+=1
+
+                params=(name,phone,price,rating,add,lat_log)
+                statement='''INSERT INTO EAT VALUES(Null,?,?,?,?,?,?)'''
+                cur.execute(statement,params)
+                conn.commit()
+
+            except:
+                pass
 
 
 
 # # 2.3lyft data
+# statement='''CREATE TABLE RIDE(
+# 'Id' INTEGER NULL PRIMARY KEY AUTOINCREMENT,
+# 'Origin'TEXT NOT NULL,
+# 'Origin_geo' TEXT NOT NULL,
+# 'Destination' TEXT NOT NULL,
+# 'Destination_geo' TEXT NOT NULL,
+# 'Estimated time (minute)' TEXT,
+# 'Estimated distance(miles)' TEXT,
+# 'Estimated maximun cost (dollar)' TEXT,
+# 'Estimated minimum cost (dollar)' TEXT)
+# '''
+# cur.execute(statement)
+# conn.commit()
+#
+# update_one='UPDATE RIDE SET Destination_geo=(SELECT lag_log FROM EAT)'
+# cur.execute(update_one)
+# conn.commit()
+#
+# update_two='UPDATE RIDE SET Destination=(SELECT address FROM EAT)'
+# cur.execute(update_two)
+# conn.commit()
+
 client_id='KZ-TZqsDDPf9'
 client_secret='K7GU9SoiwvUMcr-QyzQ_ynaqlGpEF9Ww'
 access_token='7ze30S6B+qvt4f7vet2nm6PHg8q8wfQWEIg3lxeP1EPgHoLFagPZuevnqm07lXx19gwXZZMNdhwSRuCc2yVG+NacsXJifU3M8sJyjMavSvz1t6RpLCmL8u4='
@@ -173,20 +182,9 @@ token_type='bearer'
 
 auth_flow = ClientCredentialGrant(client_id, client_secret,scope,)
 session = auth_flow.get_session()
-
-
 client = LyftRidesClient(session)
 
-#1.startpoint: nearby driver
-user_input_address="San Francisco International Airport"
-start_add=google_map.lat_log(user_input_address)
-
-statement = """
-SELECT e.lag_log
-FROM EAT as e
-"""
-end_lag_log_list=cur.execute(statement).fetchall()
-
+#1.startpoint
 
 #
 def estmate_cost(start,end,type="lyft"):
@@ -203,10 +201,7 @@ def estmate_cost(start,end,type="lyft"):
     est_dict['start']=start
     est_dict['end']=end
     est_dict['es_time']=est["estimated_duration_seconds"]/60
-
-
     est_dict['es_distance']=est["estimated_distance_miles"]
-
     est_dict['es_cost_max']=est["estimated_cost_cents_max"]/100
     est_dict['es_cost_min']=est["estimated_cost_cents_min"]/100
 
@@ -238,19 +233,22 @@ def estmate_cost(start,end,type="lyft"):
 #
 #     return est_dict
 #
-#
-# {"cost_estimates": [{"ride_type": "lyft", "estimated_duration_seconds": 2831, "estimated_distance_miles": 15.69, "price_quote_id": "6a1106b3e29ae48a99026b2e68d1df26343b25442db0b2ad3b6224d4a950b95d", "estimated_cost_cents_max": 3805, "primetime_percentage": "0%", "is_valid_estimate": true, "currency": "USD", "cost_token": null, "estimated_cost_cents_min": 3805, "display_name": "Lyft", "primetime_confirmation_token": null, "can_request_ride": true}]}
-#
-for destination in end_lag_log_list:
-    ridedb=estmate_cost(start_add,destination)
-    param=(start_add,str(destination),ridedb['es_time'],ridedb['es_distance'],ridedb['es_cost_max'],ridedb['es_cost_min'])
-    statement='''INSERT INTO RIDE VALUES(Null,?,?,?,?,?,?)'''
-    cur.execute(statement,param)
-    conn.commit()
+#start address
+user_input_address="San Francisco International Airport"
+start_add=google_map.lat_log(user_input_address)
+#destination address
+statement='select Destination_geo from RIDE'
+destination=cur.execute(statement).fetchall()
+print(destination)
+# for each in destination:
+#     ridedb=estmate_cost(start_add,each)
+#     param=(user_input_address,start_add,ridedb['es_time'],ridedb['es_distance'],ridedb['es_cost_max'],ridedb['es_cost_min'])
+#     statement='''INSERT INTO RIDE(Origin, Origin_geo, Estimated time (minute), Estimated distance(miles),Estimated maximun cost (dollar),Estimated minimum cost (dollar))
+#      VALUES(?,?,?,?,?,?)'''
+#     cur.execute(statement,param)
+#     conn.commit()
 
-# cost_resp=client.get_cost_estimates(start_latitude=37.7763, start_longitude=-122.3918, end_latitude=38.3918, end_longitude=-122.3918)
-# print(cost_resp.json["cost_estimates"])
-# cost=cost_resp.json
+
 #
 #
 # eta_resp=client.get_eta(37.7763, -122.3918)
@@ -353,150 +351,11 @@ for destination in end_lag_log_list:
 #         else:
 #             raise Exception(driver_eta_response.json())
 #
-#     def get_ride_estimates(self, start_lat, start_lng, end_lat=None, end_lng=None, ride_type=None):
-#         """
-#         A GET to the /cost endpoint returns the estimated cost, distance, and duration of a ride between
-#         a start location and end location. A success response will be broken down by ride types available
-#         at the specified location. An optional ride_type parameter can be specified to only return the
-#         cost_estimate for that ride_type. Valid inputs for the ride_type parameter can be found by querying
-#         the /v1/ridetypes endpoint.
-#         If the destination parameters are not supplied, the cost endpoint will simply return the Prime Time
-#         pricing at the specified location.
-#         :param start_lat: float, REQUIRED, Latitude of a location
-#         :param start_lng: float, REQUIRED, Longitude of a location
-#         :param end_lat: float, Latitude of a location
-#         :param end_lng: float, Longitude of a location
-#         :param ride_type: string, ID of a ride type. Returned by this endpoint
-#         :return: ride estimates in JSON format
-#         """
-#         start_lat = float(start_lat)
-#         start_lng = float(start_lng)
-#         # check if destination is given
-#         if all([end_lat, end_lng]) is True:
-#             end_lat = float(end_lat)
-#             end_lng = float(end_lng)
 #
-#         # construct the request URL
-#         if ride_type is None and (end_lat is None or end_lng is None):
-#             ride_estimates_url = "{}cost?start_lat={}&start_lng={}".format(AVAILABILITY,start_lat, start_lng)
-#
-#         elif ride_type is not None and (end_lat is None or end_lng is None):
-#             ride_estimates_url = "{}cost?start_lat={}&start_lng={}&ride_type={}".format(AVAILABILITY,start_lat, start_lng, ride_type)
-#
-#         elif ride_type is not None and (end_lat is not None or end_lng is not None):
-#             ride_estimates_url = "{}cost?start_lat={}&start_lng={}&end_lat={}&end_lng={}&ride_type={}".format(
-#                 AVAILABILITY, start_lat, start_lng, end_lat, end_lng, ride_type)
-#
-#         elif ride_type is None and (end_lat is not None or end_lng is not None):
-#             ride_estimates_url = "{}cost?start_lat={}&start_lng={}&end_lat={}&end_lng={}".format(
-#                 AVAILABILITY, start_lat, start_lng, end_lat, end_lng)
-#
-#         headers = {"Authorization": "{} {}".format(self.token_type,
-#                                                    self.__access_token)}
-#
-#         ride_estimates_response = requests.get(ride_estimates_url, headers=headers)
-#
-#         if ride_estimates_response.status_code == 200:
-#             return ride_estimates_response.json()
-#
-#         else:
-#             raise Exception(ride_estimates_response.json())
-#
-#     def get_nearby_drivers(self, lat, lng):
-#         """
-#         A GET to the /drivers endpoint returns the location of drivers near a location.
-#         The result will contain a list of 5 locations for a sample of drivers for each ride type available
-#         at the specified latitude and longitude.
-#         :param lat: float, REQUIRED, Latitude of a location
-#         :param lng: float, REQUIRED, Longitude of a location
-#         :return: nearby drivers in JSON format
-#         """
-#         lat = float(lat)
-#         lng = float(lng)
-#
-#         nearby_drivers_url = "{}drivers?lat={}&lng={}".format(AVAILABILITY, lat, lng)
-#
-#         headers = {"Authorization": "{} {}".format(self.token_type,
-#                                                    self.__access_token)}
-#
-#         nearby_drivers_response = requests.get(nearby_drivers_url, headers=headers)
-#
-#         if nearby_drivers_response.status_code == 200:
-#             return nearby_drivers_response.json()
-#
-#         else:
-#             raise Exception(nearby_drivers_response.json())
-#
-#     def get_eta_and_nearby_drivers(self, lat, lng, end_lat=None, end_lng=None, ride_type=None):
-#         """
-#         A GET to the /nearby-drivers-pickup-etas endpoint returns the estimated time for nearby
-#         drivers to reach the specified location, and their most recent locations.
-#         A success response will be broken down by ridetypes available at the specified location.
-#         An optional ride_type parameter can be specified to only return the ETA for that ridetype.
-#         Valid inputs for the ride_type parameter can be found by querying the /v1/ridetypes endpoint.
-#         An empty response indicates that the specified ride_type isn't available at the specified location.
-#         You can try requesting again without the ride_type parameter.
-#         :param lat: float, REQUIRED, Latitude of a location
-#         :param lng: float, REQUIRED, Longitude of a location
-#         :param end_lat: float, Latitude of a location
-#         :param end_lng: float, Longitude of a location
-#         :param ride_type: string, ID of a ride type. Returned by this endpoint
-#         :return: nearby drivers and eta in JSON format
-#         """
-#         lat = float(lat)
-#         lng = float(lng)
-#         # check if destination is given
-#         if all([end_lat, end_lng]) is True:
-#             end_lat = float(end_lat)
-#             end_lng = float(end_lng)
-#
-#         # construct the request URL
-#         if ride_type is None and (end_lat is None or end_lng is None):
-#             eta_and_nearby_drivers_url = "{}nearby-drivers-pickup-etas?lat={}&lng={}".format(AVAILABILITY, lat, lng)
-#
-#         elif ride_type is not None and (end_lat is None or end_lng is None):
-#             eta_and_nearby_drivers_url = "{}nearby-drivers-pickup-etas?lat={}&lng={}&ride_type={}".format(
-#                 AVAILABILITY, lat, lng, ride_type)
-#
-#         elif ride_type is not None and (end_lat is not None or end_lng is not None):
-#             eta_and_nearby_drivers_url = "{}nearby-drivers-pickup-etas?lat={}&lng={}&destination_lat={}&destination_lng={}&ride_type={}".format(
-#                 AVAILABILITY, lat, lng, end_lat, end_lng, ride_type)
-#
-#         elif ride_type is None and (end_lat is not None or end_lng is not None):
-#             eta_and_nearby_drivers_url = "{}nearby-drivers-pickup-etas?lat={}&lng={}&destination_lat={}&destination_lng={}".format(
-#                 AVAILABILITY, lat, lng, end_lat, end_lng)
-#
-#         headers = {"Authorization": "{} {}".format(self.token_type,
-#                                                    self.__access_token)}
-#
-#         eta_and_nearby_drivers_response = requests.get(eta_and_nearby_drivers_url, headers=headers)
-#
-#         if eta_and_nearby_drivers_response.status_code == 200:
-#             return eta_and_nearby_drivers_response.json()
-#
-#         else:
-#             raise Exception(eta_and_nearby_drivers_response.json())
-#
-# Availability().get_nearby_drivers(111,'123','45')
-# 2.scraping +crawling
-# baseurl="https://finance.yahoo.com/"
-# industry_url=baseurl+"industries"
-# html=make_request_using_cache(industry_url)
-#
-# soup = BeautifulSoup(html, 'html.parser')
-# # select industry
-# # print(soup.prettify())
-# industry_col=soup.find_all('div',title=" Finance")
-# industry_title_all=soup.find_all('a',class_="D(b) Td(n) Ta(start) C(black) Lh(34px)")
-# print(industry_title_all)
-# industry_url=find('a',title=)['href']
-
-
-# openfile=
-# header={'','','','','','',''}
-# 3.database
-#
-# with open (CACHE_FNAME) as f
-#     bars_dr=csv.reader(f)
-#     bars_ls=[]
 conn.close()
+
+
+
+# if __name__ == '__main__':
+#     # model.init_bball()
+#     app.run(debug=True)
